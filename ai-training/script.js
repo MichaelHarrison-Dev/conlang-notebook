f2f6e01f331bb469f5fa8df3fa6be9ba1de44b08
MichaelHarrison-Dev <michael.harrison.dev@gmail.com>
2024-07-31 09:26:14 -0600

    Update script.js

--- ai-training/script.js
+++ ai-training/script.js
@@ -25,63 +25,62 @@ window.addEventListener("keydown", event => {
     }
 });
 
-// Main Logic
+// Initialization Logic
 function initialize() {
-    let wordStructureDeclarationText = localStorage.getItem("word-structure-declaration-text");
+    let phonology = JSON.parse(localStorage.getItem("phonology"));
 
-    if (wordStructureDeclarationText == null) {
+    // set default display text
+    if (phonology == null) {
         primaryParagraph.innerText = "Your phonology is incomplete!";
         secondaryParagraph.innerText = "Before using this feature, your phonology must be completely defined";
+        return;
     } else {
-        wordStructureDeclarationText.split('\n').forEach(declaration => {
-            let checkbox = document.createElement("input");
-            checkbox.setAttribute("type", "checkbox");
-            checkbox.setAttribute("id", declaration);
-            checkbox.setAttribute("name", "word-structure-declaration");
-            checkbox.setAttribute("value", declaration);
-            checkbox.addEventListener("change", () => {
-                let checkedDeclarations = Array.from(document.getElementsByName("word-structure-declaration")).filter(x => x.checked).map(x => x.value);
-                if (checkedDeclarations.length > 0) {
-                    randomWordGenerator = createRandomWordGenerator(checkedDeclarations);
-                    currentWord = randomWordGenerator.createWord();
-                    primaryParagraph.innerText = currentWord.getSpelling();
-                    running = true;
-                } else {
-                    primaryParagraph.innerText = "Select at least one word structure";
-                    running = false;
-                }
-            });
-
-            let label = document.createElement("label");
-            label.setAttribute("for", declaration);
-            label.innerText = " " + declaration;
-
-            let listItem = document.createElement("li");
-            listItem.appendChild(checkbox);
-            listItem.appendChild(label);
-
-            wordStructureDeclarationList.append(listItem);
-        });
-
-        let validWordArrayText = localStorage.getItem("valid-word-array-text");
-        if (validWordArrayText == null) {
-            validWords = [];
-        } else {
-            validWords = JSON.parse(validWordArrayText);
-        }
-
-        let invalidWordArrayText = localStorage.getItem("invalid-word-array-text");
-        if (invalidWordArrayText == null) {
-            invalidWords = [];
-        } else {
-            invalidWords = JSON.parse(invalidWordArrayText);
-        }
-
         primaryParagraph.innerText = "Select at least one word structure";
     }
+
+    // populate word structure list
+    phonology.wordStructures.forEach(wordStructure => {
+        let declaration = "";
+        for (soundClass of wordStructure.soundClasses) {
+            declaration += soundClass.className;
+        }
+
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("name", "word-structure-declaration");
+        checkbox.setAttribute("value", JSON.stringify(wordStructure));
+        checkbox.addEventListener("change", () => {
+            let selectedWordStructures = Array.from(document.getElementsByName("word-structure-declaration"))
+                .filter(checkbox => checkbox.checked)
+                .map(checkbox => JSON.parse(checkbox.value));
+            if (selectedWordStructures.length > 0) {
+                randomWordGenerator = createRandomWordGenerator(selectedWordStructures);
+                currentWord = randomWordGenerator.createWord();
+                primaryParagraph.innerText = currentWord.getSpelling();
+                running = true;
+            } else {
+                primaryParagraph.innerText = "Select at least one word structure";
+                running = false;
+            }
+        });
+
+        let label = document.createElement("label");
+        label.setAttribute("for", declaration);
+        label.innerText = " " + declaration;
+
+        let listItem = document.createElement("li");
+        listItem.appendChild(checkbox);
+        listItem.appendChild(label);
+
+        wordStructureDeclarationList.append(listItem);
+    });
+
+    // load labeled words 
+    validWords = JSON.parse(localStorage.getItem("valid-words")) ?? [];
+    invalidWords = JSON.parse(localStorage.getItem("invalid-words")) ?? [];
 }
 
 function saveWordLists() {
-    localStorage.setItem("valid-word-array-text", JSON.stringify(validWords));
-    localStorage.setItem("invalid-word-array-text", JSON.stringify(invalidWords));
+    localStorage.setItem("valid-words", JSON.stringify(validWords));
+    localStorage.setItem("invalid-words", JSON.stringify(invalidWords));
 }
